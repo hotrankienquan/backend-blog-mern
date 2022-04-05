c2bda03610a8982a4bbe6f633bcbec3f1d1bf584
Kien Quan <hotrankienquan144@gmail.com>
2022-04-05 09:32:36 +0700

    config corrs lan 2

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 app.use(cors());
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-  });`enter code here`
-console.log('lama')
+// app.use(function(req, res, next) {
+//     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     next();
+//   });`enter code here`
+
+// console.log('lama')
+
 // app.use('/', (req, res) => {
 //   console.log('this is main url')
 //   res.send("this is main")
@@ -57,4 +59,23 @@ app.use("/api/categories", categoryRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend is running.");
+});
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
